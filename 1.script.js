92d543e70400e60c1c71ef90c1898739756f6475
Madan.patakota <madan.patakota@gmail.com>
2025-09-18 09:33:50 +0530

    first project

---
+++ 1.script.js
@@ -0,0 +1,12 @@
+var location1 = "bangolore";
+var locationmgrName = "JOHN";
+var age = 10;
+var age1 = 20;
+var age2 = 20;
+//Check the .ts file
+//=========================
+//1. as save just check the .js file
+//2. now build the project and verfy .ts
+//Question Check the .ts file
+//=========================
+//1. After save is it possible that to convert .ts file to .js file automatically?
